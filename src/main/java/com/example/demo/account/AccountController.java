76591accc77cbc0d0d3d77c275b39289945b0a6f
andrew <umanking@gmail.com>
2019-09-07 09:25:53 +0900

    Add passwordEncoder

--- src/main/java/com/example/demo/account/AccountController.java
+++ src/main/java/com/example/demo/account/AccountController.java
@@ -1,6 +1,7 @@
 package com.example.demo.account;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,12 @@ public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @GetMapping("/account/{role}/{username}/{password}")
     public Account createAccount(@ModelAttribute Account account){
-        account.setPassword("{noop}"+ account.getPassword());
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accountRepository.save(account);
     }
 }
