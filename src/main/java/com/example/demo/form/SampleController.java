d13941e6291a6ff1fe327e7919f795ecc9084ba0
andrew <umanking@gmail.com>
2019-09-07 10:46:50 +0900

    Debugging Spring Security Context Holder

--- src/main/java/com/example/demo/form/SampleController.java
+++ src/main/java/com/example/demo/form/SampleController.java
@@ -1,5 +1,6 @@
 package com.example.demo.form;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,9 @@ import java.security.Principal;
 
 @Controller
 public class SampleController {
+
+    @Autowired
+    private SampleService sampleService;
 
     @GetMapping("/")
     public String index(Model model, Principal principal){
@@ -28,6 +32,7 @@ public class SampleController {
     @GetMapping("/dashboard")
     public String dashboard(Model model, Principal principal){
         model.addAttribute("message", "Hello Dashboard: " + principal.getName());
+        sampleService.dashboard();
         return "dashboard";
     }
 
